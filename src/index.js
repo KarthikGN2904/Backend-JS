58a4c8eacdd00ca90748ff27d7100888d64936bc
KarthikGN2904 <karthikn2904@gmail.com>
2024-10-18 16:04:09 +0530

    cors and cookie-parser updates

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./.env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(` Server listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed!!!: ", err);
+  });
 
 /*
 import expreess from "express";
