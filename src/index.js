b6e914b3941579aa7433233097213fa29729f74f
KarthikGN2904 <karthikn2904@gmail.com>
2024-10-18 14:59:09 +0530

    connected to database of MONGO

---
+++ src/index.js
@@ -0,0 +1,32 @@
+//require("dotenv").config({ path: "./.env" });
+import dotenv from "dotenv";
+
+//import mongoose from "mongoose";
+//import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./.env" });
+
+connectDB();
+
+/*
+import expreess from "express";
+const app = expreess();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR CONNECTING TO DB", error);
+    throw error;
+  }
+})();
+*/
